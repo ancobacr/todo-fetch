10e2040e6260edcdd67b92c33c10e49f779af105
ancobacr <ancobacr@gmail.com>
2021-05-07 11:20:26 +0000

    fetch todo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,99 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Card, Container, Button } from "react-bootstrap";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export function ToDoList() {
+	const [userInput, setUserInput] = useState(" ");
+	const [task, setTask] = useState([]);
+	const [count, setCount] = useState(1);
 
-//create your first component
-export function Home() {
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ancoba", {
+			method: "GET",
+			headers: {
+				"Content-Type": "aplication/json"
+			}
+		})
+			.then(respond => {
+				return respond.json();
+			})
+			.then(data => {
+				setTask(task.concat(data));
+			})
+			.catch(error => console.log("error", error));
+	}, []);
+
+	function changesInList() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/ancoba", {
+			method: "PUT",
+			body: JSON.stringify(task),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok);
+				console.log(resp.status);
+				console.log(resp.text());
+				return resp.json();
+			})
+			.then(data => {
+				console.log(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
+	const handleInfo = () => {
+		if (userInput != null) {
+			let newArray = [...task, userInput];
+			setTask(newArray);
+			setUserInput(" ");
+			setCount(count + 1);
+		}
+	};
+
+	const deleteTask = id => {
+		task.splice(id, 1);
+		setTask([...task]);
+		setCount(count - 1);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<Container className="justify-content-center">
+			<Container className="text-center mt-5">
+				<Card style={{ width: "25rem" }}>
+					<Card.Title>
+						<span className="h1">Todo List</span>
+					</Card.Title>
+
+					<input
+						className="mb-2"
+						placeholder="what needs to be done?"
+						type="text"
+						requiered
+						value={userInput}
+						onChange={e => setUserInput(e.target.value)}
+						onKeyPress={e =>
+							e.key === "Enter" ? handleInfo() : ""
+						}
+					/>
+					{task.map((final, id) => (
+						<span className="card-subtitle mb-4" key={id}>
+							{final}
+							<Button
+								variant="light"
+								onClick={() => deleteTask(id)}>
+								X
+							</Button>
+						</span>
+					))}
+
+					<Card.Footer>
+						<p className="text-muted">{count} task's left</p>
+					</Card.Footer>
+				</Card>
+			</Container>
+		</Container>
 	);
 }
